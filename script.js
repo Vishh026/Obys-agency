cc869191cd371ed18c979a007b697a0caa2b64c9
Vaishnavi Dhawan <vaishnavidhavan026@gmail.com>
2024-09-04 09:47:01 +0530

    locomotive with cursor effect

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function locomotiveAnimation(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+ScrollTrigger.refresh();
+}
+
 function loadingAnimation(){
     var tl = gsap.timeline()
 tl.from(".line h1",{
@@ -51,7 +82,11 @@ tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
     y:120,
     stagger:0.2
 })
+tl.from("#hero1, #page2",{
+    opacity:0
+},"-=1.2")
 }
+
 function cursorAnimation(){
     document.addEventListener("mousemove",function(dets){
         gsap.to(".cursor",{
@@ -61,5 +96,7 @@ function cursorAnimation(){
     })
     Shery.makeMagnet(".nav-part2 h4");
 }
+
 cursorAnimation();
-loadingAnimation()
+loadingAnimation();
+locomotiveAnimation();
